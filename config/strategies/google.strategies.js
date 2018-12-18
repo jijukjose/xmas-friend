901e553d6832c2539738411643c539a8f99b3b07
jijukjose <jijukjose@gmail.com>
2018-12-18 21:29:59 +0100

    updated site

--- config/strategies/google.strategies.js
+++ config/strategies/google.strategies.js
@@ -1,12 +1,13 @@
 var passport = require('passport');
 var GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
 module.exports = function () {
+    var port = process.env.PORT||3000;
     passport.use(new GoogleStrategy({
             //clientID: '877264380453-pp1l7e8ben5oi7be150fns13a6sbml0g.apps.googleusercontent.com',
             clientID: '330971086891-15cqbie6ofp5gto6pjoke2kc672s5096.apps.googleusercontent.com',
             //clientSecret: 'MhUcl6Lc8vj3QdG4EkkgBq_m',
             clientSecret: 'I3dwktSnvSISV1Xr_iifOUvD',
-            callbackURL: 'http://guarded-plains-98992/auth/google/callback',
+            callbackURL: 'http://guarded-plains-98992:'+port+'/auth/google/callback',
         },
         function(req, accessToken, refreshToken, profile, done){
             var user = {};
